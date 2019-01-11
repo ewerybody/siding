b5d0e92c71d6cc873df0193681a2473915519930
Eric Werner <ewerybody+github@gmail.com>
2019-01-11 13:22:49 +0100

    cleaned the unittest, added nfo, added msg passing

--- test/app_test_app.py
+++ test/app_test_app.py
@@ -1,5 +1,5 @@
 try:
-    from singlesiding import QSingleApplication
+    import singlesiding
 except ModuleNotFoundError:
     print('ModuleNotFoundError ...')
     import os
@@ -8,18 +8,19 @@ except ModuleNotFoundError:
     print('    cwd: %s' % cwd)
     if cwd not in sys.path:
         sys.path.append(cwd)
-    from singlesiding import QSingleApplication
+    import singlesiding
 
 
 def main():
-    app = QSingleApplication()
-    app.setOrganizationName("test")
-    app.setApplicationName("test app")
-    app.setApplicationVersion("1")
-    app.ensure_single()
+    app = singlesiding.initialize('test', 'test app', '1', 'message!')
     return app
+
+
+def _on_msg_receive(msg):
+    print('msg: %s' % msg)
 
 
 if __name__ == '__main__':
     app = main()
+    app.message_received.connect(_on_msg_receive)
     app.exec_()
--- test/test.py
+++ test/test.py
@@ -5,38 +5,46 @@ Created on 21.06.2018
 '''
 import os
 import sys
-import json
-import uuid
 import time
 import unittest
 import subprocess
 
-import singlesiding
+# we cannot test QSingleApplication here directly because we'd need
+# to app.exec_() the thing and thus halt the process.
+# import singlesiding
 
 
 HERE = os.path.dirname(os.path.abspath(__file__))
-print('HERE: %s' % HERE)
 TEST_APP_PATH = os.path.join(HERE, 'app_test_app.py')
 
 
 class Test(unittest.TestCase):
 
     def test_app(self):
-        print('test_app ...')
-        p = subprocess.run([sys.executable, TEST_APP_PATH])
-        print('p: %s' % p)
-#        app = singlesiding.initialize('test', 'test app', '1')
-#        app.message_received.connect()
-#        app = QSingleApplication()
-#        app.setOrganizationName()
-#        app.setApplicationName()
-#        app.setApplicationVersion("1")
-#        app.ensure_single()
+        p1 = subprocess.Popen([sys.executable, TEST_APP_PATH])
+        time.sleep(0.2)
+        self.assertTrue(_is_running(p1.pid))
+        print('p1.pid: %s' % p1.pid)
+        # process 1 is onening
 
-    def test_app2(self):
-        print('test_app2 ...')
-        p = subprocess.run([sys.executable, TEST_APP_PATH])
-        print('p: %s' % p)
+        p2 = subprocess.Popen([sys.executable, TEST_APP_PATH])
+        print('p2.pid: %s' % p2.pid)
+        p2.wait(5)
+        self.assertFalse(_is_running(p2.pid))
+        # process 2 killed itself
+
+        p1.terminate()
+        time.sleep(0.2)
+        self.assertFalse(_is_running(p1.pid))
+        # process 1 was terminated
+
+
+def _is_running(pid):
+    cmd = 'wmic.exe process where processID="%s" get name /format:list' % pid
+    output = subprocess.check_output(cmd, stderr=subprocess.DEVNULL).strip()
+    if not isinstance(output, str):
+        output = output.decode()
+    return output != ''
 
 
 if __name__ == '__main__':
